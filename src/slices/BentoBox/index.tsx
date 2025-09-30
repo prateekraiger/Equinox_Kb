810df30188441d84c5344ca7dcadfb4e2c4deced
prateekraiger <prateekraiger123@gmail.com>
2025-09-30 14:55:58 +0530

    updated fadeIn component

--- src/slices/BentoBox/index.tsx
+++ src/slices/BentoBox/index.tsx
@@ -8,6 +8,7 @@ import {
 import { Bounded } from "@/components/Bounded";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
+import { FadeIn } from "@/components/FadeIn";
 
 /**
  * Props for `BentoBox`.
@@ -23,18 +24,20 @@ const BentoBox: FC<BentoBoxProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <h2
-        id="features"
-        className="font-bold-slanted mb-8 scroll-pt-6 text-6xl uppercase md:text-8xl"
-      >
-        <PrismicText field={slice.primary.heading} />
-      </h2>
+      <FadeIn>
+        <h2
+          id="features"
+          className="font-bold-slanted mb-8 scroll-pt-6 text-6xl uppercase md:text-8xl"
+        >
+          <PrismicText field={slice.primary.heading} />
+        </h2>
+      </FadeIn>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-6">
+      <FadeIn targetChildren className="grid grid-cols-1 gap-4 md:grid-cols-6">
         {slice.primary.items.map((item) => (
           <BentoBoxItem key={asText(item.text)} item={item} />
         ))}
-      </div>
+      </FadeIn>
     </Bounded>
   );
 };
